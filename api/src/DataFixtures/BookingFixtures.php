1328b5d3e3e469c32affaf426b8ed57b25613e40
Marek Kisiel <leisikkeram@gmail.com>
2020-03-26 12:22:34 +0100

    add fixtures

--- api/src/DataFixtures/BookingFixtures.php
+++ api/src/DataFixtures/BookingFixtures.php
@@ -6,28 +6,53 @@ use App\Entity\Booking;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class BookingFixtures extends Fixture
-{
-    public function load(ObjectManager $manager)
-    {
-        $booking = new Booking();
-        $booking->setBarberId(1);
-        
-        
-        $start = new \DateTime('+1 day');
-        $start->setTime(9, 30);
-        
-        $end = new \DateTime('+1 day');
-        $end->setTime(10, 0);
-        
-        $booking->setStartAt($start);
-        $booking->setEndAt($end);
-        
-        $manager->persist($booking);
+class BookingFixtures extends Fixture {
+
+    public function load(ObjectManager $manager) {
+        foreach ($this->getBookingData() as $booking_data) {
+            list($barber_id, $days_from_now, $start_hour, $start_minutes, $end_hour, $end_minutes) = $booking_data;
+            $booking = new Booking($barber_id);
+            $booking->setStartAt($this->createDateTime($days_from_now, $start_hour, $start_minutes));
+            $booking->setEndAt($this->createDateTime($days_from_now, $end_hour, $end_minutes));
+            $manager->persist($booking);
+        }
+//        $booking = new Booking(1);
+//        $booking->setStartAt($this->createDateTime(2, 9, 30));
+//        $booking->setEndAt($this->createDateTime(2, 10, 0));
+//        $manager->persist($booking);
+//
+//        $booking = new Booking(1);
+//        $booking->setStartAt($this->createDateTime(2, 10, 0));
+//        $booking->setEndAt($this->createDateTime(2, 12, 0));
+//        $manager->persist($booking);
+//
+//        $booking = new Booking(1);
+//        $booking->setStartAt($this->createDateTime(2, 10, 0));
+//        $booking->setEndAt($this->createDateTime(2, 12, 0));
+//        $manager->persist($booking);
+
+
         $manager->flush();
     }
-    
-    private function createBooking(ObjectManager $manager) {
-        
+
+    private function getBookingData() {
+        return [
+            [
+                1, 2, 9, 30, 10, 0
+            ],
+            [
+                1, 2, 10, 0, 12, 0
+            ],
+            [
+                2, 2, 11, 0, 12, 30
+            ],
+        ];
     }
-}  
+
+    private function createDateTime(int $days_from_now, int $hour, int $minutes) {
+        $dt = new \DateTime(sprintf("+$days_from_now day", $days_from_now));
+        $dt->setTime($hour, $minutes);
+        return $dt;
+    }
+
+}
